2dd9a6448fcf61fad9038e83ec04da760291bf50
Shams M. Monem <shamsmmonem@gmail.com>
2025-04-26 00:12:37 +0300

    Added Dockerfile to account, auth, seperated test and production in node.js backends, updated docker-compose.yml

--- config/config.js
+++ config/config.js
@@ -1,7 +1,25 @@
-module.exports = {
-    username: 'your_db_username',
-    password: 'your_db_password',
-    database: 'your_db_name',
-    host: 'localhost',
-    dialect: 'postgres'
-  };
+const config = {
+    development: {
+        dialect: 'postgres',
+        username: process.env.POSTGRES_USER || 'postgres',
+        host: process.env.POSTGRES_HOST || '127.0.0.1',
+        database: process.env.POSTGRES_DATABASE || 'postgres',
+        password: process.env.POSTGRES_PASSWORD || 'password',
+        port: process.env.POSTGRES_PORT || 5432,
+    },
+    production: {
+        dialect: 'postgres',
+        username: process.env.POSTGRES_USER || 'postgres',
+        host: process.env.POSTGRES_HOST || '127.0.0.1',
+        database: process.env.POSTGRES_DATABASE || 'postgres',
+        password: process.env.POSTGRES_PASSWORD || 'password',
+        port: process.env.POSTGRES_PORT || 5432,
+    },
+    test: {
+        dialect: 'sqlite',
+        storage: ':memory:',
+        logging: false,
+    },
+}
+
+module.exports = config[process.env.NODE_ENV || 'development'];
