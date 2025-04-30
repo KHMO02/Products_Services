438e84160747e3a160e13ed256867b55bf697d2f
Khaled Mohamed Mohamed Ali <khaled.mohamed02@outlook.com>
2025-04-30 14:01:26 +0300

    All tests Passed

--- services/productService.js
+++ services/productService.js
@@ -93,7 +93,7 @@ module.exports = {
     await ProductTransfer.create({
       product_id: productId,
       buyer_id: userId,
-      transfer_date: new Date()
+      date_time: new Date()
     });
     
 
